ba48a7df4d11204c7d38ba1da49223d974adb862
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-09-26 19:06:02 +0600

    Done

---
+++ app/src/main/java/com/example/tip/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.tip
+
+
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.example.tip.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val price: EditText = findViewById(R.id.price)
+        val button: Button = findViewById(R.id.button)
+        val radio: RadioGroup = findViewById(R.id.rad)
+
+        button.setOnClickListener {
+            if (price.text.isEmpty()){
+                Toast.makeText(this,"Empty cost", Toast.LENGTH_SHORT).show()
+            }else {
+                val percent = when (radio.checkedRadioButtonId) {
+                            R.id.exc -> 0.20
+                            R.id.good -> 0.18
+                            else -> 0.15
+                        }
+                val priceVal = price.text.toString().toDouble()
+                binding.tipAmount.text = getString(R.string.tip_amount, price.text)
+                Toast.makeText(
+                    this, (priceVal + calc(priceVal,percent)).toString(), Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+    }
+    fun calc(price:Double , tipPercent:Double):Double{
+        return ((price*tipPercent))
+    }
+}
