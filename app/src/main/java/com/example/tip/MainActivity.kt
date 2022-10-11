2a14f50681fe33e30cbfb6211a181c8156123933
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-10-11 08:29:37 +0600

    main logic done, works correctly

--- app/src/main/java/com/example/tip/MainActivity.kt
+++ app/src/main/java/com/example/tip/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tip
 
 import android.app.ProgressDialog.show
+import android.content.res.Resources
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -23,16 +24,15 @@ class MainActivity : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val price: EditText = findViewById(R.id.price)
         val button: Button = findViewById(R.id.button)
         val radio: RadioGroup = findViewById(R.id.rad)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        binding.
-        button.setOnClickListener {
-            Toast.makeText(this,"Empty cost", Toast.LENGTH_SHORT).show()
 
+        button.setOnClickListener {
             if (price.text.isEmpty()){
                 Toast.makeText(this,"Empty cost", Toast.LENGTH_SHORT).show()
             }else {
@@ -42,17 +42,13 @@ class MainActivity : AppCompatActivity() {
                             else -> 0.15
                         }
                 val priceVal = price.text.toString().toDouble()
-                val calculated = NumberFormat.getCurrencyInstance().format(calc(priceVal, percent))
                 binding.tipAmount.text = getString(R.string.tip_amount, price.text)
                 Toast.makeText(
                     this, (priceVal + calc(priceVal,percent)).toString(), Toast.LENGTH_SHORT
                 ).show()
-
-//                Toast.makeText(this, binding.tipAmount.text,Toast.LENGTH_SHORT).show()
-
-
             }
         }
+
     }
     fun calc(price:Double , tipPercent:Double):Double{
         return ((price*tipPercent))
