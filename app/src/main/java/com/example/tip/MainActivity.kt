b6b795d8661e0019b95a0654043d12ee5b76a14a
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-10-11 08:24:45 +0600

    Binding done

--- app/src/main/java/com/example/tip/MainActivity.kt
+++ app/src/main/java/com/example/tip/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.annotation.RequiresApi
 import com.example.tip.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     lateinit var binding: ActivityMainBinding
+
     @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,16 +24,17 @@ class MainActivity : AppCompatActivity() {
         val price: EditText = findViewById(R.id.price)
         val button: Button = findViewById(R.id.button)
         val radio: RadioGroup = findViewById(R.id.rad)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
         button.setOnClickListener {
-            Toast.makeText(this, calc(price.text.toString().toInt(), when(radio.checkedRadioButtonId){
+            Toast.makeText(this, (price.text.toString().toDouble() + calc(price.text.toString().toInt(), when(radio.checkedRadioButtonId){
                 R.id.exc -> 0.20
                 R.id.good -> 0.18
                 else -> 0.15
-            }),Toast.LENGTH_SHORT).show()
+            })).toString(), Toast.LENGTH_SHORT).show()
+            binding.tipAmount.text = getString(R.string.tip_amount,price)
         }
     }
-    fun calc(price:Int , tipPercent:Double):String{
-        return (price+(price*tipPercent)).toString()
+    fun calc(price:Int , tipPercent:Double):Double{
+        return ((price*tipPercent))
     }
 }
