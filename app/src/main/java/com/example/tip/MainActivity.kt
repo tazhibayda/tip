3b1770a55735a0a16f941558e382759861c21e08
tazhibayda <78952869+tazhibayda@users.noreply.github.com>
2022-10-11 08:18:14 +0600

    created Action

--- app/src/main/java/com/example/tip/MainActivity.kt
+++ app/src/main/java/com/example/tip/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.tip
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val price: View = requireViewById(R.id.price)
+        val button: Button = requireViewById(R.id.button)
+        val radio: RadioGroup = requireViewById(R.id.rad)
+        val selected: RadioButton = requireViewById(radio.checkedRadioButtonId)
+
+
+        button.setOnClickListener {
+            val toast: Toast = Toast(this,)
+        }
     }
 }
